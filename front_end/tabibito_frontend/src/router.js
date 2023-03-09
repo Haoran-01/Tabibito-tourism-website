ad09a7d94c128e87bc9a2468b5a1bc46d632fa0e
JiaheZhang <jiahe.zhang@ucdconnect.ie>
2023-03-09 18:52:35 +0800

    do not push

--- front_end/tabibito_frontend/src/router.js
+++ front_end/tabibito_frontend/src/router.js
@@ -1,12 +1,12 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 // import loginView from "./components/LoginView/loginView.vue";
 // import ourTeamView from "./components/AboutUsView/ourTeamView.vue";
-// import overheardFromTravelersView from "./components/AboutUsView/overheardFromTravelersView.vue";
-import footerView from "./components/GeneralComponents/footerView.vue";
+import overheardFromTravelersView from "./components/AboutUsView/overheardFromTravelersView.vue";
+// import footerView from "./components/GeneralComponents/footerView.vue";
 
 
 const routes = [
-    { path: '/', component: footerView},
+    { path: '/', component: overheardFromTravelersView},
 ]
 
 const router = createRouter({
