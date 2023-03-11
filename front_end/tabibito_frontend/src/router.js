b11099497c555dbbe7ce1c8d22193e700f6bc334
AllenPrincipalCoder <1104028870@qq.com>
2023-03-11 13:05:04 +0800

    黄飞赫 3.11

--- front_end/tabibito_frontend/src/router.js
+++ front_end/tabibito_frontend/src/router.js
@@ -1,21 +1,23 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
-import loginView from "./components/LoginView/loginView.vue";
+// import loginView from "./components/LoginView/loginView.vue";
 import registerView from "./components/RegisterView/registerView.vue";
 import loginView from "./components/LoginView/loginView.vue";
-import navigationBar from "./components/GeneralComponents/navigationBar.vue";
-import aboutUsView from "./components/AboutUsView/aboutUsView.vue";
+import forgetPasswordView from "./components/ForgetPasswordView/forgetPasswordView.vue";
+// import navigationBar from "./components/GeneralComponents/navigationBar.vue";
+// import aboutUsView from "./components/AboutUsView/aboutUsView.vue";
 // import loginView from "./components/LoginView/loginView.vue";
 // import ourTeamView from "./components/AboutUsView/ourTeamView.vue";
-import overheardFromTravelersView from "./components/AboutUsView/overheardFromTravelersView.vue";
+// import overheardFromTravelersView from "./components/AboutUsView/overheardFromTravelersView.vue";
 // import footerView from "./components/GeneralComponents/footerView.vue";
 
 
 const routes = [
     { path: '/', component: loginView },
     { path: '/register', component: registerView },
+    { path: '/forget', component: forgetPasswordView},
 
-    { path: '/', component: footerView},
-    { path: '/', component: overheardFromTravelersView},
+    // { path: '/', component: footerView},
+    // { path: '/', component: overheardFromTravelersView},
 ]
 
 const router = createRouter({
