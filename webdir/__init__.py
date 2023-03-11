3c81bd775848c5b5487c1dd7210ba4821d6c6dab
Jerry <wenjie.wu@ucdconnect.ie>
2023-03-11 13:07:36 +0800

    登陆注册忘记密码功能的后端部分

--- webdir/__init__.py
+++ webdir/__init__.py
@@ -1,16 +1,14 @@
 import os
-from flask import Flask, jsonify
+from flask import jsonify
 from wtforms import ValidationError
 from flask_login import LoginManager
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail
-#from models import
-#from blueprints
+from models import User
 
 from flask import Flask
 from config import config
 from exts import mail, db
+from blueprints import user_bp
 
 
 def create_app(config_name):
@@ -24,6 +22,23 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 db.init_app(app)
 mail.init_app(app)
 migrate = Migrate(app, db)
+app.register_blueprint(user_bp)
 
 app.secret_key = os.getenv("SECRET_KEY", "mou107b6vsfxor82bbc4bzf4mcu7")
 
+login_manager = LoginManager()
+login_manager.login_message_category = 'info'
+login_manager.login_message = 'Access denied'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_email):
+    if User.query.filter_by(user_email=user_email).first() is not None:
+        curr_user = User()
+        curr_user.user_email = user_email
+        return curr_user
+
+
+@app.errorhandler(ValidationError)
+def validation_error(e):
+    return jsonify({"code": 400, "message": e.args})
