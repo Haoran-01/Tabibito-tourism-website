ccc99f31ead1ad6b21e1310d8f9ca996432c6ed9
JiaheZhang <jiahe.zhang@ucdconnect.ie>
2023-03-11 20:12:13 +0800

    do not push

--- front_end/tabibito_frontend/src/router.js
+++ front_end/tabibito_frontend/src/router.js
@@ -2,16 +2,14 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 import loginView from "./components/LoginView/loginView.vue";
 import registerView from "./components/RegisterView/registerView.vue";
 import aboutUsView from "./components/AboutUsView/aboutUsView.vue";
-import test from "./components/ForgetPasswordView/test.vue";
 import forgetPasswordView from "./components/ForgetPasswordView/forgetPasswordView.vue";
 import resetView from "./components/ForgetPasswordView/resetView.vue";
 
 
 const routes = [
-    { path: '/', component: loginView },
+    { path: '/', component: aboutUsView },
     { path: '/register', component: registerView },
     { path: '/forget', component: forgetPasswordView},
-    { path: '/test', component: test},
     { path: '/about', component: aboutUsView},
     { path: '/reset', component: resetView},
 
