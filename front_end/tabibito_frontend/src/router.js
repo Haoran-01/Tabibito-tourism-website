bd38e1d549ef1374741d9a4b2856e826ca8ae99a
JiaheZhang <jiahe.zhang@ucdconnect.ie>
2023-03-13 16:00:24 +0800

    do not push

--- front_end/tabibito_frontend/src/router.js
+++ front_end/tabibito_frontend/src/router.js
@@ -4,10 +4,11 @@ import registerView from "./components/RegisterView/registerView.vue";
 import aboutUsView from "./components/AboutUsView/aboutUsView.vue";
 import forgetPasswordView from "./components/ForgetPasswordView/forgetPasswordView.vue";
 import resetView from "./components/ForgetPasswordView/resetView.vue";
+import mostPopularTours from "./components/HomePageView/mostPopularTours.vue";
 
 
 const routes = [
-    { path: '/', component: loginView },
+    { path: '/', component: mostPopularTours },
     { path: '/register', component: registerView },
     { path: '/forget', component: forgetPasswordView},
     { path: '/about', component: aboutUsView},
