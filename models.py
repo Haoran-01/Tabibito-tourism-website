789294d59f51c4b17706539226a199bacf69387f
Jerry <wenjie.wu@ucdconnect.ie>
2023-03-29 11:53:33 +0800

    search and recommend

--- models.py
+++ models.py
@@ -7,16 +7,15 @@ from sqlalchemy.orm import relationship
 
 class User(UserMixin, db.Model):
     __tablename__ = 'user'
-    user_email = db.Column(db.CHAR(200), primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_email = db.Column(db.CHAR(200), unique=True)
     user_first_name = db.Column(db.CHAR(200), nullable=False)
     user_last_name = db.Column(db.CHAR(200), nullable=False)
     user_password = db.Column(db.CHAR(200), nullable=False)
 
     comments = relationship('Comment', back_populates="user")
     profile = relationship('UserProfile', uselist=False, back_populates="user")
-
-    def get_id(self):
-        return self.user_email
+    user_browses = relationship('UserBrowse', back_populates='user')
 
     def __repr__(self):
         return "<User(email='%s')>" % self.user_email
@@ -26,7 +25,7 @@ class UserProfile(db.Model):
     __tablename__ = 'user_profile'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     picture_address = db.Column(db.CHAR(200))
-    user_email = db.Column(db.CHAR(200), ForeignKey('user.user_email', ondelete='CASCADE', onupdate='CASCADE'))
+    user_id = db.Column(db.Integer, ForeignKey('user.user_id', ondelete='CASCADE', onupdate='CASCADE'))
     user = relationship('User', back_populates="profile")
 
 
@@ -52,10 +51,11 @@ class Product(db.Model):
 
     comments = relationship('Comment', order_by='Comment.id', back_populates="product")
     trips = relationship('Trip', order_by='Trip.id', back_populates="product")
-
     fee_des = relationship('FeeDes', order_by='FeeDes.id', back_populates="product")
     tags = relationship('Tag', order_by='Tag.id', back_populates="product")
     pictures = relationship('ProductPicture', order_by='ProductPicture.id', back_populates="product")
+    user_browses = relationship('UserBrowse', order_by='UserBrowse.id', back_populates='product')
+
 
     def __repr__(self):
         return "<Product(name='%s')>" % self.name
@@ -119,7 +119,7 @@ class Comment(db.Model):
     des = db.Column(db.Text, nullable=False)
     like_num = db.Column(db.Integer, default=0)
     product_id = db.Column(db.Integer, ForeignKey('product.id', ondelete='CASCADE', onupdate='CASCADE'))
-    user_email = db.Column(db.CHAR(200), ForeignKey('user.user_email', ondelete='CASCADE', onupdate='CASCADE'))
+    user_id = db.Column(db.Integer, ForeignKey('user.user_id', ondelete='CASCADE', onupdate='CASCADE'))
 
     product = relationship('Product', back_populates="comments")
     user = relationship('User', back_populates="comments")
@@ -136,3 +136,30 @@ class CommentPicture(db.Model):
 
     comment_id = db.Column(db.Integer, ForeignKey('comment.id', ondelete='CASCADE', onupdate='CASCADE'))
     comment = relationship('Comment', back_populates="pictures")
+
+
+class UserBrowse(db.Model):
+    __tablename__ = 'user_browse'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, ForeignKey('user.user_id'), nullable=False)
+    product_id = db.Column(db.Integer, ForeignKey('product.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    duration = db.Column(db.Integer, nullable=False)
+
+    # Define relationships
+    user = relationship('User', back_populates='user_browses')
+    product = relationship('Product', back_populates='user_browses')
+
+    def __init__(self, user_id, product_id, duration):
+        self.user_id = user_id
+        self.product_id = product_id
+        self.duration = duration
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'product_id': self.product_id,
+            'created_at': self.created_at,
+            'duration': self.duration
+        }
