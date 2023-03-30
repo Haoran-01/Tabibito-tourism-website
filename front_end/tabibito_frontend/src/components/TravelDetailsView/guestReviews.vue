c6134078f16bd38000831a5910e8139f4f3f26e5
JiaheZhang <jiahe.zhang@ucdconnect.ie>
2023-03-30 09:41:51 +0800

    2023-03-30 ZJH ini

---
+++ front_end/tabibito_frontend/src/components/TravelDetailsView/guestReviews.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "guestReviews"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ front_end/tabibito_frontend/src/components/TravelDetailsView/leaveReply.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "leaveReply"
+}
+</script>
+
+<style scoped>
+
+</style>
