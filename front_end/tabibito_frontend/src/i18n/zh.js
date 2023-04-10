d341fff3861b55cbd40af6ee5b03cb0fe6ac074d
Wenbei Xie <wenbei.xie@ucdconnect.ie>
2023-04-10 17:35:37 +0800

    improved Chinese support

--- front_end/tabibito_frontend/src/i18n/zh.js
+++ front_end/tabibito_frontend/src/i18n/zh.js
@@ -20,5 +20,79 @@ export default {
         logout: "登出",
         login: "登录",
         signup: "注册"
+    },
+    homepage: {
+        searchPart:{
+            slogan1: "最好的",
+            slogan2: "旅游体验",
+            sloganDescription: "体验各种令人兴奋的旅游和旅行套餐，并进行酒店预订、寻找度假套餐、搜索便宜的酒店和活动",
+            loc: "目的地",
+            stet: "开始日期 - 结束日期",
+            st: "开始日期",
+            et: "结束日期",
+            tags: "标签",
+            button: "搜索",
+        },
+        discountPart:{
+            title: "优惠和折扣",
+            slogan: "我们为您提供以下优惠",
+            period: "享受春季优惠",
+            bookTitle: "尽早预定来享受优惠",
+            button: "现在预定",
+        },
+        figurePart:{
+            reviews: "评价",
+            travelProjects: "旅行项目",
+            happyCustomers: "满意的顾客",
+            orders: "订单"
+        },
+        whyChoosePart:{
+            title: "为什么选择我们",
+            slogan: "有很多可供选择的热门目的地",
+            attr1: "精准信息保障",
+            val1: "我们尽力提供有关旅行的准确信息。",
+            attr2: "简单快捷的预订",
+            val2: "只需几步，您就可以轻松预订新的行程。",
+            attr3: "24/7 全天候客户服务",
+            val3: "每一位顾客都可以获得顾客服务。",
+
+        }
+    },
+    aboutUsPage: {
+        mainSlogan: "追寻旅行的乐趣？",
+        subSlogan: "您值得信赖的旅行网站",
+        whyChooseTitle: "为什么选择我们",
+        whyChooseSlogan: "有很多可供选择的热门目的地",
+        advantageName1: "精准信息保障",
+        advantageText1: "我们尽力提供有关旅行的准确信息",
+        advantageName2: "简单快捷的预订",
+        advantageText2: "只需几步，您就可以轻松预订新的行程。",
+        advantageName3: "24/7 全天候客户服务",
+        advantageText3: "每一位顾客都可以获得顾客服务。",
+        companyIntroTitle: "关于Tabibito",
+        companyIntroSlogan: "有很多可供选择的热门目的地",
+        intro1: "欢迎来到 Tabibito，满足您所有假期需求的一站式旅行社。 我们的名字在日语中的意思是“旅行者”，我们以帮助我们的客户轻松舒适地探索世界而自豪。",
+        intro2: "在 Tabibito，我们了解计划假期可能会让人不知所措。 这就是我们提供精确信息保证的原因 - 我们为您提供有关您的目的地的准确和最新信息，以便您可以就您的行程做出明智的决定。 我们还提供轻松快捷的预订服务，让您轻松、放心地预订您的旅行。",
+        intro3: "我们对客户满意度的承诺是坚定不移的，我们提供 24/7 全天候客户服务，以确保您的旅行顺利进行。 无论您需要预订方面的帮助、对行程有疑问，还是在旅途中需要帮助，我们的团队都会随时为您提供帮助。",
+        intro4: "我们提供广泛的旅游服务，包括机票、住宿、旅游等。 我们的专业旅行顾问可以根据您的喜好和预算帮助您规划完美的假期。",
+        intro5: "Tabibito致力于为我们的客户提供难忘的旅行体验。 让我们帮助您踏上下一次冒险之旅——立即联系我们，开始规划您的梦想假期。",
+    },
+    fourZeroFourPage:{
+        slogan: "哎呀！看来你迷路了。",
+        subSlogan: "您要查找的页面不可用。 尝试再次搜索或使用下方按钮。",
+        button: "返回首页"
+    },
+    fourZeroThreePage:{
+        slogan: "哎呀！看来你被阻止了。",
+        subSlogan: "您要查找的页面暂时不可用。 尝试再次搜索或使用下方按钮。",
+        button: "返回首页"
+    },
+    projectDetailPage:{
+        ChargeList: {title: "收费详情"},
+        itinerary: {
+            title: "行程",
+            seeDetail: "查看详情",
+            collapse: "收起"
+        },
     }
 }
