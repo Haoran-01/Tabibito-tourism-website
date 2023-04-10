811a2312ccf160db1c9f64608d3341853dcb360b
Donglin Liu <donglin.liu@ucdconnect.ie>
2023-04-10 10:52:27 +0800

    完成中英文切换的配置

---
+++ front_end/tabibito_frontend/src/i18n/zh.js
@@ -0,0 +1,24 @@
+export default {
+    login: {
+        welcome: '欢迎',
+        noAccount: "还没有账号?",
+        signup: "免费注册",
+        password: "密码",
+        email: "邮箱",
+        forget: "忘记密码？",
+        signin: "登录",
+        signin2: "或用如下方式登录",
+        google: "谷歌",
+        agree: "通过创建帐户，说明您同意我们的服务条款和隐私声明。"
+    },
+    navi: {
+        lang: "中文",
+        USD: "美元",
+        CNY: "人民币",
+        EUR: "欧元",
+        profile: "个人资料",
+        logout: "登出",
+        login: "登录",
+        signup: "注册"
+    }
+}
