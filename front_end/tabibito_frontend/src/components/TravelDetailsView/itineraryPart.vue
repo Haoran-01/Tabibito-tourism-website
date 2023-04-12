edfab2a15d14aa261a5d617213c8b3198e25c9f8
Wenbei Xie <wenbei.xie@ucdconnect.ie>
2023-04-12 14:43:46 +0800

    improved Google map support

--- front_end/tabibito_frontend/src/components/TravelDetailsView/itineraryPart.vue
+++ front_end/tabibito_frontend/src/components/TravelDetailsView/itineraryPart.vue
@@ -21,18 +21,61 @@ export default {
   mounted() {
     const loader = new Loader({
       apiKey: "AIzaSyBctzU8ocpP_0j4IdTRqA-GABIAnaXd0ow",
-      version: "weekly",
+      version: "beta",
+      libraries: ["marker"],
+      language: "en-US"
     });
 
-    loader.load().then(() => {
-      const Beijing = { lat: 40, lng: 116 };
+    loader.load().then((google) => {
+      const center = { lat: 34.84555, lng: -111.8035 };
       let map = new google.maps.Map(document.getElementById("map"), {
-        center: Beijing,
-        zoom: 8,
+        center: center,
+        zoom: 12,
+        mapId: "fdhssdfkhsdjkhasdfjkh"
       });
-      const marker = new google.maps.Marker({
-        position: Beijing,
-        map: map,
+      const tourStops = [
+        {
+          position: { lat: 34.8791806, lng: -111.8265049 },
+          title: "Boynton Pass",
+        },
+        {
+          position: { lat: 34.8559195, lng: -111.7988186 },
+          title: "Airport Mesa",
+        },
+        {
+          position: { lat: 34.832149, lng: -111.7695277 },
+          title: "Chapel of the Holy Cross",
+        },
+        {
+          position: { lat: 34.823736, lng: -111.8001857 },
+          title: "Red Rock Crossing",
+        },
+        {
+          position: { lat: 34.800326, lng: -111.7665047 },
+          title: "Bell Rock",
+        },
+      ];
+      // Create an info window to share between markers.
+      const infoWindow = new google.maps.InfoWindow();
+      tourStops.forEach(({ position, title }, i) => {
+        const pinView = new google.maps.marker.PinView({
+          glyph: `${i + 1}`,
+        });
+        const marker = new google.maps.marker.AdvancedMarkerView({
+          position,
+          map,
+          title: `${i + 1}. ${title}`,
+          content: pinView.element,
+        });
+
+        // Add a click listener for each marker, and set up the info window.
+        marker.addListener("click", ({ domEvent, latLng }) => {
+          const { target } = domEvent;
+
+          infoWindow.close();
+          infoWindow.setContent(marker.title);
+          infoWindow.open(marker.map, marker);
+        });
       });
 
     });
