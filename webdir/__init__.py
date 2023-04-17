c16ebe87db4bafa0987ba007dcacd41de6486efc
阎浩然 <yhr1019@163.com>
2023-04-17 22:11:31 +0800

    修bug

--- webdir/__init__.py
+++ webdir/__init__.py
@@ -20,11 +20,11 @@ def create_app(config_name):
     # nltk.download('stopwords')
     # nltk.download('wordnet')
 
-    app = Flask(__name__, template_folder="templates", static_folder="static", static_url_path="")
+    app = Flask(__name__, template_folder="templates/dist", static_folder="templates/dist", static_url_path="")
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
-    CORS(app, supports_credentials=True)
+    # CORS(app, supports_credentials=True)
     return app
 
 # nltk.download('stopwords')
@@ -83,16 +83,16 @@ def context_processor():
         return {}
 
 
-@app.after_request
-def af_req(resp):  # 解决跨域session丢失
-    resp = make_response(resp)
-    resp.headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:5173'
-    resp.headers['Access-Control-Allow-Methods'] = 'PUT,POST,GET,DELETE,OPTIONS'
-    resp.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
-    resp.headers[
-        'Access-Control-Allow-Headers'] = 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild'
-    resp.headers['Access-Control-Allow-Credentials'] = 'true'
-
-    resp.headers['X-Powered-By'] = '3.2.1'
-    resp.headers['Content-Type'] = 'application/json;charset=utf-8'
-    return resp
+# @app.after_request
+# def af_req(resp):  # 解决跨域session丢失
+#     resp = make_response(resp)
+#     resp.headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:5173'
+#     resp.headers['Access-Control-Allow-Methods'] = 'PUT,POST,GET,DELETE,OPTIONS'
+#     resp.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+#     resp.headers[
+#         'Access-Control-Allow-Headers'] = 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild'
+#     resp.headers['Access-Control-Allow-Credentials'] = 'true'
+#
+#     resp.headers['X-Powered-By'] = '3.2.1'
+#     resp.headers['Content-Type'] = 'application/json;charset=utf-8'
+#     return resp
