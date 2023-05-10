0a9c3561ecdc14249f491ed18ddc8e62e2e8d185
Jerry <wenjie.wu@ucdconnect.ie>
2023-05-10 13:33:27 +0800

    后端准备文件

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from webdir import app
+from flask import render_template
 import logging
+
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.debug = True
