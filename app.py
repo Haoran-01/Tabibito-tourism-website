f6b21938ac4b3b8b9170c2a6821f8d57b415711e
阎浩然 <yhr1019@163.com>
2023-05-11 16:58:03 +0800

    修bug

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+from flask_wtf.csrf import generate_csrf
+
 from webdir import app
 from flask import render_template
 import logging
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', csrf_token = generate_csrf())
 
 if __name__ == '__main__':
     app.debug = True
