82cbe46a32f9b507b79cff692f902141544ea398
阎浩然 <yhr1019@163.com>
2023-05-11 19:35:59 +0800

    修bug

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask_wtf.csrf import generate_csrf
-
 from webdir import app
 from flask import render_template
-import logging
+
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    return render_template('index.html', csrf_token = generate_csrf())
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.debug = True
--- config.py
+++ config.py
@@ -22,8 +22,6 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SESSION_COOKIE_SAMESITE = None
     SESSION_COOKIE_SECURE = True
-    # 后加的
-    SESSION_COOKIE_HTTPONLY = True
 
     # 数据库的配置变量
     HOSTNAME = '127.0.0.1'
