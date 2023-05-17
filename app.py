1764418bbda32587485c92ad809f306060bd60fa
Jerry <wenjie.wu@ucdconnect.ie>
2023-05-17 12:59:08 +0800

    数据库

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from webdir import app
-from flask import render_template
+from flask import render_template, send_from_directory
 
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
-
+@app.route('/pictures/<path:filename>')
+def get_picture(filename):
+    return send_from_directory('website/pictures', filename)
 
 if __name__ == '__main__':
     app.debug = True
